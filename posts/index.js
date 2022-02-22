c9916579f1c7e7c1f1283b1d8a5ff43d000c8877
Hieu Nguyen <hieunguyen@Hieus-MacBook-Pro.local>
2022-02-22 13:23:30 +0700

    implemented posts service

--- posts/index.js
+++ posts/index.js
@@ -1,1 +1,34 @@
+const express = require('express');
+const crypto = require('crypto');
+const cors = require('cors');
 
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+// GLOBAL MIDDLEWARE
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+// DATA
+const posts = {};
+
+// ROUTE HANDLING
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/posts', (req, res) => {
+  const { title } = req.body;
+
+  const postId = crypto.randomBytes(4).toString('hex');
+
+  posts[postId] = { id: postId, title };
+
+  res.send(posts[postId]);
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
